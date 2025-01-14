b54f0fd1eac5d23922641e88b06dc2e02a6998c8
Eric Korber <erickorber1994@gmail.com>
2025-01-14 17:20:41 +0900

    Load product info from db on product page

--- app/routes/product.$productId.tsx
+++ app/routes/product.$productId.tsx
@@ -1,17 +1,40 @@
-export default function Product() {
+import { LoaderFunction, redirect } from "@remix-run/node";
+import { json, useLoaderData } from "@remix-run/react";
+import { prisma } from "~/db.server";
 
-    const testProduct = {
-        id: 3,
-        productName: "Pokemonsss",
-        price: 19.99,
-        imgUrl: "https://m.media-amazon.com/images/I/61MVcnn+3oL._AC_UL320_.jpg"
+// Define the type of data being returned from the loader
+interface Product {
+    id: string;
+    name: string;
+    price: number;
+    stock: number;
+    description: string;
+    imageUrl: string;
+    rating: number;
+    numRatings: number;
+}
+
+export const loader: LoaderFunction = async ({ params }) => {
+    const product = await prisma.product.findUnique({where: { id: params.productId }});
+    
+    if (!product) {
+        // Throw 404 error if product is not found
+        throw new Response('Product Not Found', { status: 404 });
     }
+    
+    return json({product});
+};
+
+export default function Product() {
+    const { product } = useLoaderData<{ product: Product }>();
 
     return (
         <div>
-            <img src={testProduct.imgUrl} alt={testProduct.productName} />
-            <h2>{testProduct.productName}</h2>
-            <p>${testProduct.price.toFixed(2)}</p>
+            <img src={product.imageUrl} alt={product.name} />
+            <h2>{product.name}</h2>
+            <p>${product.price.toFixed(2)}</p>
+            <p>Stock: {product.stock}</p>
+            <p>{product.description}</p>
         </div>
     );
 }
