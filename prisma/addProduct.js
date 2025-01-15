d41cb6f7bb2a2b208f2d25acb8dd4715112b1fee
Eric Korber <erickorber1994@gmail.com>
2025-01-15 11:26:22 +0900

    Added a second product to db

--- prisma/addProduct.js
+++ prisma/addProduct.js
@@ -5,10 +5,10 @@ const prisma = new PrismaClient();
 async function main() {
   const newProduct = await prisma.product.create({
     data: {
-      name: 'Pokemon Cards',
-      description: 'This is a description of the new pokemon Cards',
-      price: 19.99,
-      imageUrl: 'https://m.media-amazon.com/images/I/61MVcnn+3oL._AC_UL320_.jpg'
+      name: 'Single Yugioh Card',
+      description: 'This is a new Yugioh Card',
+      price: 14.99,
+      imageUrl: 'https://m.media-amazon.com/images/I/81uTgCY9W3L._AC_UL320_.jpg'
     },
   });
 
