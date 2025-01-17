74ddd21caebeb7ebf261076d7b0ee835ba4fd05b
Eric Korber <erickorber1994@gmail.com>
2025-01-17 13:43:50 +0900

    Add total cart quantity to header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,13 +1,27 @@
 import { Link } from "@remix-run/react";
+import { useCartContext } from "~/contexts/CartContext";
 
 export default function Header() {
+    const { cartItems } = useCartContext();
+
+    const calculateTotalItems = () => {
+        let total = 0;
+        cartItems.forEach(item => {
+            total += item.quantity
+        });
+
+        return total
+    }
+
+    const totalItems = calculateTotalItems();
+
     return (
         <div>
             <Link to={"/"}>Japan Delivered</Link>
             <p> </p>
             <Link to={"/products"}>Products</Link>
             <p> </p>
-            <Link to={"/cart"}>Cart</Link>
+            <Link to={"/cart"}>Cart {totalItems > 0 && totalItems}</Link>
             <p> </p>
         </div>
     );
