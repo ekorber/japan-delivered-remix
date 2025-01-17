75108638f88ba672fe5556ddab81e477372ca62f
Eric Korber <erickorber1994@gmail.com>
2025-01-17 13:12:56 +0900

    Add quantity dropdown to cart

--- app/components/cart-item.tsx
+++ app/components/cart-item.tsx
@@ -10,9 +10,27 @@ interface CartItemProps {
 }
 
 export default function CartListItem({ id, quantity, name, price, description, imageUrl, updateQuantity, removeFromCart } : CartItemProps) {
+    
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = parseInt(event.target.value, 10);
+        updateQuantity(id, value);
+    };
+
     return (
         <div>
-            <p>{name} : {quantity}</p>
+            <p>{name} - ${(price * quantity).toFixed(2)}</p>
+            <div>
+                <select className="numberDropdown" value={quantity} onChange={handleChange}>
+                    <option value="" disabled>
+                    -- Select a number --
+                    </option>
+                    {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+                        <option key={num} value={num}>
+                            {num}
+                        </option>
+                    ))}
+                </select>
+            </div>
             <button onClick={() => removeFromCart()}>Remove From Cart</button>
         </div>
     );
